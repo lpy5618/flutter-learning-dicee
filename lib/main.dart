8a90a7e7d46b9db406ab59eb717e9cc02d0236f8
lpy <lpy547898@gmail.com>
2023-04-06 17:17:58 +1000

    finish

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdicenumber = 1;
+  int rightdicenumber = 2;
+  void randomDices() {
+    setState(() {
+      leftdicenumber = Random().nextInt(6) + 1;
+      rightdicenumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                randomDices();
+              },
+              child: Image.asset('images/dice$leftdicenumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                randomDices();
+              },
+              child: Image.asset('images/dice$rightdicenumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
